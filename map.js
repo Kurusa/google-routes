e581bf614d1037cacd5e12eaa72c949285a8b5da
Kurusa <51157689+Kurusa@users.noreply.github.com>
2025-01-11 19:56:42 +0200

    updates

--- map.js
+++ map.js
@@ -4,37 +4,24 @@ $(document).ready(function () {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    var markers = L.markerClusterGroup();
+    map.addLayer(markers);
+
     function convertE7(value) {
         return value / 10000000;
     }
 
-    function visualizeDataOnMap(data) {
-        var routesHash = {};
-
-        function createRouteHash(startCoords, endCoords) {
-            return startCoords.toString() + '_' + endCoords.toString();
-        }
-
-        data.timelineObjects.forEach(function (timelineObject) {
-            if (!timelineObject.activitySegment) {
-                return;
-            }
+    function visualizeDataOnMapChunk(chunk) {
+        chunk.forEach(function (timelineObject) {
+            if (!timelineObject.activitySegment) return;
 
             var segment = timelineObject.activitySegment;
             var startCoords = [convertE7(segment.startLocation.latitudeE7), convertE7(segment.startLocation.longitudeE7)];
             var endCoords = [convertE7(segment.endLocation.latitudeE7), convertE7(segment.endLocation.longitudeE7)];
 
-            var routeHash = createRouteHash(startCoords, endCoords);
-
-            if (routesHash[routeHash]) {
-                return;
-            }
-
-            routesHash[routeHash] = true;
-
             var polylinePoints = [startCoords];
 
-            if (segment.waypointPath && segment.waypointPath.waypoints) {
+            if (segment.waypointPath?.waypoints) {
                 segment.waypointPath.waypoints.forEach(function (waypoint) {
                     polylinePoints.push([convertE7(waypoint.latE7), convertE7(waypoint.lngE7)]);
                 });
@@ -42,63 +29,59 @@ $(document).ready(function () {
 
             polylinePoints.push(endCoords);
 
-            L.polyline(polylinePoints, {color: 'blue'}).addTo(map);
-
-            L.marker(startCoords).addTo(map);
-            L.marker(endCoords).addTo(map);
+            L.polyline(polylinePoints, { color: 'blue' }).addTo(map);
+            markers.addLayer(L.marker(startCoords));
+            markers.addLayer(L.marker(endCoords));
         });
+    }
 
-        if (Object.keys(routesHash).length > 0) {
-            var allRoutes = [];
-            for (var route in routesHash) {
-                var points = route.split('_').map(function (point) {
-                    return point.split(',').map(function (coord) {
-                        return parseFloat(coord);
-                    });
-                });
-                allRoutes = allRoutes.concat(points);
+    function processLargeDataInChunks(data, chunkSize) {
+        let index = 0;
+
+        function processChunk() {
+            const chunk = data.slice(index, index + chunkSize);
+            visualizeDataOnMapChunk(chunk);
+            index += chunkSize;
+
+            if (index < data.length) {
+                setTimeout(processChunk, 0);
             }
-            var bounds = L.latLngBounds(allRoutes);
-            map.fitBounds(bounds);
         }
+
+        processChunk();
     }
 
     function processFiles(files) {
-        var readFilesPromises = Array.from(files).map(file => {
+        var fileReaders = Array.from(files).map(file => {
             if (file.name.endsWith('.json')) {
                 return new Promise((resolve, reject) => {
                     var reader = new FileReader();
                     reader.onload = e => {
                         try {
-                            var data = JSON.parse(e.target.result);
-                            resolve(data); // Розв'язання проміса з даними
+                            resolve(JSON.parse(e.target.result));
                         } catch (error) {
-                            console.error("Error reading or parsing file", error);
-                            reject(error); // Відхилення проміса у разі помилки
+                            console.error("Error parsing file", error);
+                            reject(error);
                         }
                     };
-                    reader.onerror = error => {
-                        console.error("Error reading file", error);
-                        reject(error);
-                    };
+                    reader.onerror = reject;
                     reader.readAsText(file);
                 });
             }
-        }).filter(promise => promise !== undefined);
+        }).filter(Boolean);
 
         map.eachLayer(function (layer) {
-            if (!!layer.toGeoJSON) {
+            if (layer instanceof L.Marker || layer instanceof L.Polyline) {
                 map.removeLayer(layer);
             }
         });
+        markers.clearLayers();
 
-        Promise.all(readFilesPromises).then(allData => {
+        Promise.all(fileReaders).then(allData => {
             allData.forEach(data => {
-                visualizeDataOnMap(data);
+                processLargeDataInChunks(data.timelineObjects, 100); // Чанки по 100 елементів
             });
-        }).catch(error => {
-            console.error("Error processing files", error);
-        });
+        }).catch(console.error);
     }
 
     $('#uploadForm').on('submit', function (e) {
@@ -106,13 +89,13 @@ $(document).ready(function () {
         var files = $('#jsonInput').get(0).files;
         if (files.length > 0) {
             processFiles(files);
-            var label = files.length === 1 ? files[0].name : files.length + " files";
+            var label = files.length === 1 ? files[0].name : `${files.length} files`;
             $('.custom-file-label').text(label);
         }
     });
 
     $('.custom-file-input').on('change', function (e) {
-        var fileLabel = e.target.files.length === 1 ? e.target.files[0].name : e.target.files.length + " files";
+        var fileLabel = e.target.files.length === 1 ? e.target.files[0].name : `${e.target.files.length} files`;
         $(this).next('.custom-file-label').addClass("selected").html(fileLabel);
     });
 });
